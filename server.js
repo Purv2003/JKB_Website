fe69bf3cf4ca3acdce76502fa93f60a0d3423ead
Purv2003 <103986991+Purv2003@users.noreply.github.com>
2023-08-08 10:29:00 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const handlerBar = require("express-handlebars");
+const path = require('path')
+const app = express();
+//const __dirname = path.resolve(path.dirname(''));
+const port = 3000;
+
+app.use(express.static("public"));
+app.use("/jkb_logo.ico", express.static("images/jkb_logo.ico"));
+
+app.engine("handlebars", handlerBar.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views")
+
+app.get("/", (req, res) => {
+    res.render("home", { layout: false, title:"Home",
+    helpers:{
+        listName() {
+            let names = ["Purva", "Chiranjeev", "Khushi", "Swaraj"];
+            let html = names
+            .map((names) => {
+                return `<li>${names}</li>`;
+            })
+            .join("\n");
+            return html;
+        },
+           
+    },
+
+});
+    
+});
+
+app.get("/about", (req, res) => {
+    res.render("about", { layout: false,title:"About",
+     
+ });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+/*app.use(express.static("public"));
+app.use("/jkb_logo.ico", express.static("images/jkb_logo.ico"));
+
+app.engine("handlebars", handleBar.engine());
+app.set("view engine", "handlebars");
+app.set("views", ".")
+
+app.get("/",function (req,res){
+   // res.send('hello')
+    res.sendFile(path.join(__dirname+'/Homepage.html'))
+})
+
+app.get('/admission',function (req,res){
+    //res.send('about');
+    res.sendFile(path.join(__dirname+'/engg_admin.html'))
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+app.listen(port, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running, and App is
+                      listening on port "+ port)
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
+*/
